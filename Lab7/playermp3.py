dc2d46bc2aa39949976bbd2282dd52fa8ede7df6
cod3xc <arslan.alisher@gmail.com>
2025-10-30 17:48:16 +0500

    Autoupdate==: 2025-10-30 17:48:16

--- Lab7/playermp3.py
+++ Lab7/playermp3.py
@@ -1,15 +1,15 @@
 import pygame
 import os
 
-musicpath = "./Lab7"
-tracks = [f for f in os.listdir(musicpath)
-          if f.lower().endswith((".mp3"))]
 pygame.init()
 pygame.mixer.init()
 screen = pygame.display.set_mode((500, 200))
 pygame.display.set_caption("Player: SPACE=Play/Pause, Arrowleft/Arrowright=Prev/Next")
+musicpath = "./Lab7"
+tracks = [a for a in os.listdir(musicpath)
+          if a.lower().endswith((".mp3"))]
 clock = pygame.time.Clock()
-a = 0              
+b = 0              
 playing = False    
 paused = False
 running = True
@@ -28,19 +28,19 @@ while running:
                     paused = True
                     playing = False
                 else:
-                    pygame.mixer.music.load(os.path.join(musicpath, tracks[a]))
+                    pygame.mixer.music.load(os.path.join(musicpath, tracks[b]))
                     pygame.mixer.music.play()
                     playing = True
                     paused = False
             elif music.key == pygame.K_RIGHT:
-                a = (a+1) % len(tracks)
-                pygame.mixer.music.load(os.path.join(musicpath, tracks[a]))
+                b = (b+1) % len(tracks)
+                pygame.mixer.music.load(os.path.join(musicpath, tracks[b]))
                 pygame.mixer.music.play()
                 playing = True
                 paused = False
             elif music.key == pygame.K_LEFT:
-                a = (a-1) % len(tracks)
-                pygame.mixer.music.load(os.path.join(musicpath, tracks[a]))
+                b = (b-1) % len(tracks)
+                pygame.mixer.music.load(os.path.join(musicpath, tracks[b]))
                 pygame.mixer.music.play()
                 playing = True
                 paused = False
